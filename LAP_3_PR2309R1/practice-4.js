7e35578fb436cc8556f868210c35cefaa0956b0e
hoanglong0512 <62041670+hoanglong0512@users.noreply.github.com>
2023-11-08 15:14:30 +0700

    updated lap3

---
+++ LAP_3_PR2309R1/practice-4.js
@@ -0,0 +1,18 @@
+// Bài 4: Cho 2 array có cùng số phần tử với nhau. 
+let array1 = [10, 20, 30, 40];
+let array2 = [100, 200, 300, 400];
+
+// đảo chiều.
+array2.reverse();
+
+for (let i in array1) {
+    document.write(`${array1[i]} ${array2[i]}` + '<br>')
+}
+
+// Hãy in các thành phần trong 2 array theo format dưới đây (sử dụng document.write)
+/**
+ * 10 400
+ * 20 300
+ * 30 200
+ * 40 100
+ */
